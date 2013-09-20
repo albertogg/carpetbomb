11fd8a5b01ce1d0cec1a7cd2e21b218f7c3d9f45
albertogg <albertogrespan@gmail.com>
2013-09-20 12:43:01 -0430

    Testing some slight modifications.

--- lib/carpetbomb/engine.rb
+++ lib/carpetbomb/engine.rb
@@ -35,10 +35,10 @@ end
 handler = CarpetBomb::Handler.new
 
 [:md, :mdown, :markdown].each do |extension|
-  if defined? ActionView::Template::Handlers and ActionView::Template::Handlers.respond_to? :register_template_handler
-    ActionView::Template::Handlers
-  elsif defined? ActionView::Template and ActionView::Template.respond_to? :register_template_handler
+  if defined? ActionView::Template and ActionView::Template.respond_to? :register_template_handler
     ActionView::Template
+  elsif defined? ActionView::Template::Handlers and ActionView::Template::Handlers.respond_to? :register_template_handler
+    ActionView::Template::Handlers
   else
     raise "Couldn't find `register_template_handler' method in ActionView module."
   end.register_template_handler(extension, handler)
